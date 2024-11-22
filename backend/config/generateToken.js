66061ea6fd8c0fd25c7d6e7acf03aa55a1f9cef2
Sandeep Singh <snayal50@gmail.com>
2024-11-23 00:14:34 +0530

    added registration controller

---
+++ backend/config/generateToken.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import UserModel from '../models/user.model';
+
+const generateToken = async (email, res) => {
+  try {
+    const user = await UserModel.findOne({ email });
+    
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const payload = {
+      id: user._id,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: '7d',
+    });
+
+    // Define cookie options
+    const options = {
+      httpOnly: true, 
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: 'strict', 
+      maxAge: 7 * 24 * 60 * 60 * 1000, 
+    };
+
+    res.cookie('token', token, options);
+
+  } catch (err) {
+    console.log('Error generating token:', err.message);
+    throw err;
+  }
+};
+
+export default generateToken;
--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,2 +1,54 @@
 import UserModel from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import generateToken from "../config/generateToken.js";
 
+//register user
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check for all fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all fields!",
+        success: false,
+      });
+    }
+
+    // Check if email already exists
+    const emailExists = await UserModel.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({
+        message: "Email already registered!",
+        success: false,
+      });
+    }
+
+    // Hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    // Generate token
+    generateToken(email, res);
+
+    return res.status(200).json({
+      success: true,
+      message: "Registration Successful",
+      user: { name: user.name },
+    });
+  } catch (err) {
+    console.error("Error during user registration:", err);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
